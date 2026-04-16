42888bcea5a1f064e94404794deaaba4a795b91b
gonatural555-hue <go.natural.555@gmail.com>
2026-04-17 00:35:26 +0200

    lol

--- lib/pdp-star-products.ts
+++ lib/pdp-star-products.ts
@@ -1,5 +1,5 @@
 /**
- * PDP “star” / hero ancho: fotos horizontales (gafas, zapatillas, etc.) sin recorte agresivo.
+ * PDP “star” / hero ancho: fotos horizontales (gafas, zapatillas, ski, etc.) sin recorte agresivo.
  * Desktop: más columna de galería + ProductImageGallery heroWide; móvil: showFullImage (ver ProductDetailClient).
  */
 export const PDP_HERO_WIDE_PRODUCT_IDS = new Set<string>([
@@ -7,4 +7,5 @@ export const PDP_HERO_WIDE_PRODUCT_IDS = new Set<string>([
   "gn-cycling-011",
   "gn-cycling-shoes-002",
   "gn-mtb-shoes-001",
+  "gn-ski-snow-001",
 ]);
